d16833fef6e0a9355dbbe5526dfae66a1afb9a25
Kouadio Martial <kouadio1992@gmail.com>
2017-01-14 16:41:07 +0000

    creation des view

--- public_html/coeur/modules/administration/views/_menu_sidebar_left.php
+++ public_html/coeur/modules/administration/views/_menu_sidebar_left.php
@@ -9,114 +9,48 @@
                         <li class="nav-item">
                             <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-palette"></i>&nbsp;<span>Dashboard</span></a>
                             
-                            <ul>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/index-2.html">Dashboard v1</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/dashboard-v2.html">Dashboard v2</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/dashboard-v3.html">Dashboard v3</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/dashboard-v4.html">Dashboard v4</a></li>
-                            </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="widgets.html"><i class="zmdi zmdi-apps"></i>&nbsp;<span>Widgets</span></a>
+                            <a href="<?php echo base_url() ?>index.php/administration/info_entreprise"><i class="zmdi zmdi-apps"></i>&nbsp;<span>Infos entreprise</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-chart"></i>&nbsp;<span>Charts</span></a>
-                            <ul>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/chart-js-charts.html">ChartJS</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/flot-charts.html">Flot</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/other-charts.html">Inline Charts</a></li>
-                            </ul>
-                        </li>
+                        
                         <li class="nav-item mm-col-3">
-                            <a href="#" data-dp-action="submenu-toggle" class="mega-menu-toggle"><i class="zmdi zmdi-laptop-mac"></i>&nbsp;<span>UI Elements</span></a>
+                            <a href="#" data-dp-action="submenu-toggle" class="mega-menu-toggle"><i class="zmdi zmdi-laptop-mac"></i>&nbsp;<span>Personnels</span></a>
                             <ul class="mega-menu">
                                 <li>
                                     <ul>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/accordions.html">Accordions</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/alerts-dialogs.html">Alerts & Dialogs</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/buttons.html">Buttons</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/breadcrumbs.html">Breadcrumbs</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/colors.html">Colors</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/dropdown.html">Dropdown</a></li>
-                                    </ul>
-                                    <ul>  
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/icons.html">Icons</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/jumbotron.html">Jumbotron</a></li> 
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/notifications.html">Notifications</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/media.html">Media</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/modals.html">Modals</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/pagination.html">Pagination</a></li>
-                                    </ul>
-                                    <ul> 
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/popovers.html">Popovers</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/preloaders-progress-bar.html">Preloaders & Progress Bar</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/tabs.html">Tabs</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/tooltips.html">Tool Tips</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/typography.html">Typography</a></li>
+                                        <li><a href="<?php echo base_url() ?>index.php/administration/ajout_unite">Ajout d'une unite</a></li>
+                                        <li><a href="<?php echo base_url() ?>index.php/administration/liste_unite">liste des unites</a></li>
+                                        <li><a href="<?php echo base_url() ?>index.php/administration/ajout_personnel">Ajouter personnel</a></li>
+                                        <li><a href="<?php echo base_url() ?>index.php/administration/liste_des_personnels">liste des personnels</a></li>
+                                        
                                     </ul>
                                 </li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-border-color"></i>&nbsp;<span>Forms</span></a>
+                            <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-border-color"></i>&nbsp;<span>Valeurs</span></a>
                             <ul>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/ajout_unite">Ajout d'une unite</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/advanced-elements.html">Advanced Elements</a></li>
-                               <!--  <li><a href="<?php echo base_url() ?>index.php/administration/inscription">Inscription</a></li> -->
-                                <li><a href="<?php echo base_url() ?>index.php/administration/inscription_2">Inscription </a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/liste_unite">liste des unites</a></li>
+                                
+                                <li><a href="<?php echo base_url() ?>index.php/administration/ajout_valeur">Ajouter valeur</a></li>
+                                        <li><a href="<?php echo base_url() ?>index.php/administration/liste_des_valeurs">Liste des valeurs</a></li>
+                                
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-grid"></i>&nbsp;<span>Tables</span></a>
+                            <a href="<?php echo base_url() ?>index.php/administration/questionnaires" data-dp-action="submenu-toggle"><i class="zmdi zmdi-grid"></i>&nbsp;<span>Questionnaires</span></a>
         
-                            <ul>
-                                <li><a href="tables.html">Simple Tables</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/administration/attente_de_validation">Attente de validation</a></li>
-                            </ul>
+                            
                         </li>
                         <li class="nav-item mm-col-2">
-                            <a href="#" data-dp-action="submenu-toggle" class="mega-menu-toggle"><i class="zmdi zmdi-copy"></i>&nbsp;<span>Sample Pages</span></a>
-                            <ul class="mega-menu">
-                                <li>
-                                    <ul>
-                                    	<li><a href="<?php echo base_url() ?>index.php/administration/user-profile.html">User Profile</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/login">Login</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/signup.html">Sign Up</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/pricing-table.html">Pricing Table</a></li>
-                                    </ul>
-                                    <ul>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/invoice.html">Invoice</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/help-faqs.html">Help & FAQs</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/404.html">404</a></li>
-                                        <li><a href="<?php echo base_url() ?>index.php/administration/500.html">500</a></li>
-                                    </ul>
-                       			</li>
-                            </ul>
+                            <a href="<?php echo base_url() ?>index.php/administration/evaluation" data-dp-action="submenu-toggle" class="mega-menu-toggle"><i class="zmdi zmdi-copy"></i>&nbsp;<span>Evaluations</span></a>
+                            
                         </li>
                         <li class="nav-item">
-                            <a href="#" data-dp-action="submenu-toggle"><i class="zmdi zmdi-layers"></i>&nbsp;<span>Multi Level</span></a>
-        
-                            <ul>
-                                <li><a href="#">Level One</a></li>
-                                <li class="nav-item level-one">
-                                    <a href="#" data-dp-action="submenu-toggle">Level One</a>
-        
-                                    <ul>
-                                        <li><a href="#">Level Two</a></li>
-                                        <li><a href="#">Level Two</a></li>
-                                        <li><a href="#">Level Two</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Level One</a></li>
-                            </ul>
+                            <a href="<?php echo base_url() ?>index.php/administration/performance" data-dp-action="submenu-toggle"><i class="zmdi zmdi-layers"></i>&nbsp;<span>Performances</span></a>
+         
                         </li>
-                        <li class="nav-item">
-                            <a href="<?php echo base_url() ?>index.php/administration/pbase-classes.html"><i class="zmdi zmdi-code-setting"></i>&nbsp;<span>Base Classes</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?php echo base_url() ?>index.php/administration/documentation.html"><i class="zmdi zmdi-collection-text"></i>&nbsp;<span>Documentation</span></a>
-                        </li>
+                        
                     </ul>
                 </div>
             </nav>
