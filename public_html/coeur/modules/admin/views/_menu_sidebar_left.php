832608f33444ea2ddba28af76077cf35b8785502
Kouadio Martial <kouadio1992@gmail.com>
2017-01-14 15:12:59 +0000

    organisation

--- public_html/coeur/modules/admin/views/_menu_sidebar_left.php
+++ public_html/coeur/modules/admin/views/_menu_sidebar_left.php
@@ -23,8 +23,8 @@
                         <li class="nav-item">
                             <a href="#" data-dp-action="submenu-toggle" class="mega-menu-toggle"><i class="zmdi zmdi-laptop-mac"></i>&nbsp;<span>Souscription</span></a>
                             <ul>
-                                <li><a href="<?php echo base_url() ?>index.php/admin/souscription">souscription</a></li>
-                                <li><a href="<?php echo base_url() ?>index.php/admin/attente_de_validation_souscription">En attente de validation</a></li>
+                                <li><a href="<?php echo base_url() ?>index.php/admin/souscription">Ajouter une société</a></li>
+                                <li><a href="<?php echo base_url() ?>index.php/admin/attente_de_validation_souscription">Société en attente de validation</a></li>
                                 <li><a href="<?php echo base_url() ?>index.php/admin/liste_des_souscription_valide">Liste des souscriptions validées</a></li>
                                
                                 
